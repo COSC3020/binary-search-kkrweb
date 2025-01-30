1bb5af82345701921525bfa99bdd7f7810c4c2c6
kkrweb <kanemkriz@gmail.com>
2025-01-29 18:36:26 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,32 @@
-function binarySearch(list, element) {
-    return -1;
+// Kane Kriz
+// COSC 3020
+// Binary Search Exercise
+// 29 January 2025
+//
+
+function binarySearch(list, element) 
+{
+//assumed sorted array input
+    var arrLen = list.length();
+    midPos = Math.floor(arrLen/2);
+
+    if(list[midPos] == element)
+    {
+        return midPos;
+    }
+    else if(element > (list[midPos]))
+    {
+        binarySearch(list.slice(midPos+1, arrLen), element);
+    }
+    else if(element < (list[midPos]))
+    {
+        binarySearch(list.slice(0, midPos-1), element);
+    }
+
+        //all fails
+    else if(arrLen == 0)
+    {
+        return -1;
+    }
+    
 }
