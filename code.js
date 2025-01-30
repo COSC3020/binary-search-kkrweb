f0ffa7ef9fa29131306cd32a3359adc9025a0fa9
kkrweb <kanemkriz@gmail.com>
2025-01-29 18:54:16 -0700

    Update code.js

--- code.js
+++ code.js
@@ -7,26 +7,29 @@
 function binarySearch(list, element) 
 {
 //assumed sorted array input
-    var arrLen = list.length();
-    midPos = Math.floor(arrLen/2);
 
-    if(list[midPos] == element)
-    {
-        return midPos;
-    }
-    else if(element > (list[midPos]))
-    {
-        binarySearch(list.slice(midPos+1, arrLen), element);
-    }
-    else if(element < (list[midPos]))
-    {
-        binarySearch(list.slice(0, midPos-1), element);
-    }
+    startIndex = 0;
+    len = list.length();
+    endIndex = list.length()--;
 
-        //all fails
-    else if(arrLen == 0)
-    {
-        return -1;
+    while (startIndex <= len) 
+        {
+        midIndex = Math.floor((startIndex + len)/ 2);
+        if(list[midIndex] == element) 
+        {
+            return midIndex; //if equal, return index
+        } 
+        else if(element > list[midIndex]) 
+        {
+            startIndex = midIndex++; //focus on right side of sorted list, larger
+        } 
+        else 
+        {
+            endIndex = midIndex--; //focus on left side of sorted list, smaller values
+        }
     }
     
+    //if it exits the while, element is not within the list
+    //assuming that the list was properly sorted in the first place
+    return -1;
 }
