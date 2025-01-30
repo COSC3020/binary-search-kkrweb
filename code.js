e5bc7dbf9aca8741af0af138494132badb12e835
kkrweb <kanemkriz@gmail.com>
2025-01-29 20:07:46 -0700

    Update code.js

--- code.js
+++ code.js
@@ -3,38 +3,33 @@
 // Binary Search Exercise
 // 29 January 2025
 //
-
 function binarySearch(list, element) 
 {
     //assumed sorted list
     var arrLen = list.length;
 
-        if(arrLen == 0)
+    if (arrLen == 0) 
     {
-        return -1;
+        return -1; 
     }
-    
+
     var midPos = Math.floor(arrLen / 2);
     var midVal = list[midPos];
 
-
     if (midVal == element) 
     {
-        return midPos;
-    } 
-    
-    if (element > midVal) 
+        return midPos; //found, returns index of match
+    }
+
+    if (element > midVal)
     {
         var result = binarySearch(list.slice(midPos + 1), element);
-        if (result >= 0) 
+        if (result == -1)
         {
-            return result + midPos + 1;
-        } 
-        else
-        {
-            return -1; //not found
+            return -1; //element not found
         }
-    } 
-    
+        return midPos + 1 + result; // Adjust index to account for slicing
+    }
+
     return binarySearch(list.slice(0, midPos), element);
 }
