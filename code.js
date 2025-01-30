db0fa43dd44ce50694f55380ebe9b10a6f25dffa
kkrweb <kanemkriz@gmail.com>
2025-01-29 19:54:08 -0700

    Update code.js

--- code.js
+++ code.js
@@ -6,6 +6,30 @@
 
 function binarySearch(list, element) 
 {
-//nuking this as it got awful and I lost all brain function to debug
+    //assumed sorted array input
+    var arrLen = list.length;
+    var midPos = Math.floor(arrLen/2);
+    if(list[midPos] == element)
+    {
+        return midPos;
+    }
+    else if(element > list[midPos])
+    {
+        var result = binarySearch(list.slice(midPos+1), element);
+        if (result >= 0)
+        {
+            return result + midPos + 1;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    else if(element < list[midPos])
+    {
+        return binarySearch(list.slice(0, midPos), element);
+    }
+    //all fails
     return -1;
 }
+
